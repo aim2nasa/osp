28a5e44244867a5e24753bf3c144537ba66123bd
Rossi Kwak <rossi.kwak@gmail.com>
2018-06-04 18:11:22 +0900

    include dds related codes to see what compiler errors are to be displayed

--- study/helloWorld/HelloWorldPub.cpp
+++ study/helloWorld/HelloWorldPub.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <gen/HelloWorldData_DCPS.hpp>
 
 int main(int argc,char* argv[])
 {
+  dds::domain::DomainParticipant dp(0);
+
   std::cout<<"end of main"<<std::endl;
   return 0;
 }
