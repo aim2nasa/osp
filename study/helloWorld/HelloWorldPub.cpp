cc03e60067a17d6b9d31f0190d6cf5cfd4209948
Rossi Kwak <rossi.kwak@gmail.com>
2018-06-05 15:35:19 +0900

    publisher writes message

--- study/helloWorld/HelloWorldPub.cpp
+++ study/helloWorld/HelloWorldPub.cpp
@@ -5,6 +5,11 @@ int main(int argc,char* argv[])
 {
   dds::domain::DomainParticipant dp(0);
   dds::topic::Topic<HelloWorldData::Msg> topic(dp,"Message");
+  dds::pub::Publisher pub(dp);
+  dds::pub::DataWriter<HelloWorldData::Msg> dw(pub,topic);
+
+  HelloWorldData::Msg message(1,"Publisher's message");
+  dw.write(message);
 
   std::cout<<"end of main"<<std::endl;
   return 0;
