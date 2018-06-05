75972ca6332482cfcc057879aa2fab2e01ae2d37
Rossi Kwak <rossi.kwak@gmail.com>
2018-06-05 16:09:05 +0900

    let subscriber wait until new sample arrives infinitely

--- study/helloWorld/HelloWorldSub.cpp
+++ study/helloWorld/HelloWorldSub.cpp
@@ -10,15 +10,19 @@ int main(int argc,char* argv[])
   dds::sub::Subscriber sub(dp);
   dds::sub::DataReader<HelloWorldData::Msg> dr(sub,topic);
 
-  std::cout<<"sleeping..."<<std::endl;
-  std::this_thread::sleep_for(std::chrono::seconds(10));
-  
-  std::cout<<"reading..."<<std::endl;
-  auto samples = dr.read();
-  std::for_each(samples.begin(),samples.end(),[](const dds::sub::Sample<HelloWorldData::Msg>& s) {
-    std::cout<<"message received"<<std::endl;
-  });
-  std::cout<<"reading done"<<std::endl;
+  dds::core::cond::WaitSet ws;
+  dds::sub::cond::ReadCondition rc(dr,dds::sub::status::DataState::new_data());
+  ws += rc;
+
+  while(true) {
+    std::cout<<"waiting..."<<std::endl;
+    ws.wait();
+    auto samples = dr.read();
+    std::for_each(samples.begin(),samples.end(),[](const dds::sub::Sample<HelloWorldData::Msg>& s) {
+      std::cout<<"message received"<<std::endl;
+    });
+    std::cout<<"reading done"<<std::endl;
+  }
 
   std::cout<<"end of main"<<std::endl;
   return 0;
