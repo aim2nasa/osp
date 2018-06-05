75d5dfd590be49d7989d235bb7bf0536e255d2fa
Rossi Kwak <rossi.kwak@gmail.com>
2018-06-05 15:21:18 +0900

    use helloWorldData type for the first time in HelloWorldPub.cpp and let the library to be linked with helloPub. It requires to use some compile options(such as c++11) for successfule library link. For this osmacros is included to use 'DEFINITONS'

--- study/helloWorld/HelloWorldPub.cpp
+++ study/helloWorld/HelloWorldPub.cpp
@@ -4,6 +4,7 @@
 int main(int argc,char* argv[])
 {
   dds::domain::DomainParticipant dp(0);
+  dds::topic::Topic<HelloWorldData::Msg> topic(dp,"Message");
 
   std::cout<<"end of main"<<std::endl;
   return 0;
