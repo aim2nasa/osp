1280a9380eef2c99fc0e35d3b8b1fd9144df4837
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-01 17:10:23 +0900

    add usage

--- study/helloWorld/HelloWorldPub.cpp
+++ study/helloWorld/HelloWorldPub.cpp
@@ -3,6 +3,11 @@
 
 int main(int argc,char* argv[])
 {
+  if(argc<2) {
+    std::cout<<"usage: HelloWorldPub <message>"<<std::endl;
+    return -1;
+  }
+
   std::cout<<"pub end"<<std::endl;
   return 0;
 }
