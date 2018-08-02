6495fc9bb13fac99c4b34cfbbd8f0307f988ec67
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-02 17:33:24 +0900

    by linking lHelloWorldData undefined reference to HelloWorldData::MsgTypeSupport::~MsgTypeSupport() error is removed

--- study/helloWorld/HelloWorldPub.cpp
+++ study/helloWorld/HelloWorldPub.cpp
@@ -9,6 +9,7 @@ int main(int argc,char* argv[])
   }
 
   dds::domain::DomainParticipant dp(0);
+  dds::topic::Topic<HelloWorldData::Msg> topic(dp,"Message");
 
   std::cout<<"pub end"<<std::endl;
   return 0;
