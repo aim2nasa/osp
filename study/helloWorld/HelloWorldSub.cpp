4eee4dd4ad079f5298d79096f6285b1b9ed9bf02
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-03 11:49:31 +0900

    topic object creation, compilation successful

--- study/helloWorld/HelloWorldSub.cpp
+++ study/helloWorld/HelloWorldSub.cpp
@@ -4,6 +4,8 @@
 int main(int argc,char* argv[])
 {
   dds::domain::DomainParticipant dp(0);
+  dds::topic::Topic<HelloWorldData::Msg> topic(dp,"Message");
+
   std::cout<<"sub end"<<std::endl;
   return 0;
 }
