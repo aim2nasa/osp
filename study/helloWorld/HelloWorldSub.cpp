dc2754a7b7b3bb8de628cb0d647b5b99230fce88
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-03 11:50:32 +0900

    subscriber object creation, compilation successful

--- study/helloWorld/HelloWorldSub.cpp
+++ study/helloWorld/HelloWorldSub.cpp
@@ -5,6 +5,7 @@ int main(int argc,char* argv[])
 {
   dds::domain::DomainParticipant dp(0);
   dds::topic::Topic<HelloWorldData::Msg> topic(dp,"Message");
+  dds::sub::Subscriber sub(dp);
 
   std::cout<<"sub end"<<std::endl;
   return 0;
