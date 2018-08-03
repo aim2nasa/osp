f4db4539c6dc157ed7a3947b531c39e0217395b9
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-03 11:51:34 +0900

    data reader object creation, compilation successful

--- study/helloWorld/HelloWorldSub.cpp
+++ study/helloWorld/HelloWorldSub.cpp
@@ -6,6 +6,7 @@ int main(int argc,char* argv[])
   dds::domain::DomainParticipant dp(0);
   dds::topic::Topic<HelloWorldData::Msg> topic(dp,"Message");
   dds::sub::Subscriber sub(dp);
+  dds::sub::DataReader<HelloWorldData::Msg> dr(sub,topic);
 
   std::cout<<"sub end"<<std::endl;
   return 0;
