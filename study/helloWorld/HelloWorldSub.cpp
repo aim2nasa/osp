064999d47761fbedb01bb32d877e5c306e68b71f
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-03 11:48:22 +0900

    call dds domainparticipant ok

--- study/helloWorld/HelloWorldSub.cpp
+++ study/helloWorld/HelloWorldSub.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <gen/HelloWorldData_DCPS.hpp>
 
 int main(int argc,char* argv[])
 {
+  dds::domain::DomainParticipant dp(0);
   std::cout<<"sub end"<<std::endl;
   return 0;
 }
