b1ec58dfa8ae074c3755eeb23b0e1fa44ec15c6f
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-13 14:52:31 +0900

    create dds instances

--- study/bcast/sub.cpp
+++ study/bcast/sub.cpp
@@ -4,6 +4,9 @@
 int main(int argc,char* argv[])
 {
 	dds::domain::DomainParticipant dp(0);
+	dds::topic::Topic<bcast::Data> topic(dp,"bCast");
+	dds::sub::Subscriber sub(dp);
+	dds::sub::DataReader<bcast::Data> dr(sub,topic);
 
 	std::cout<<"sub end"<<std::endl;
 	return 0;
