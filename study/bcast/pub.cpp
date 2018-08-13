77e1e4867a26a005e2bd0a2e9604146a42c1d62a
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-13 17:56:37 +0900

    let pub read payload size and message from the user input

--- study/bcast/pub.cpp
+++ study/bcast/pub.cpp
@@ -5,7 +5,7 @@
 int main(int argc,char* argv[])
 {
 	if(argc<2) {
-		std::cout<<"usage: pub <message>"<<std::endl;
+		std::cout<<"usage: pub <payloadSize>"<<std::endl;
 		return -1;
 	}
 
@@ -14,14 +14,25 @@ int main(int argc,char* argv[])
 	dds::pub::Publisher pub(dp);
 	dds::pub::DataWriter<bcast::Data> dw(pub,topic);
 
-	unsigned long payloadSize = 4096;
+	unsigned long payloadSize = atoi(argv[1]);
+	std::cout<<"given payloadSize="<<payloadSize<<std::endl;
 	bcast::Data sample;
-	sample.id(0);
 	for(unsigned long i=0;i<payloadSize;i++) sample.payload().push_back(0x0);
 	std::cout<<"sample payload size="<<sample.payload().size()<<std::endl;
 
-	dw.write(sample);
-	std::cout<<"DW:"<<sample<<" done"<<std::endl;
+	unsigned long long id=0;
+	std::string msg;
+	while(true){
+		std::cout<<"(q to quit) message=?";
+		std::cin>>msg;
+
+		if("q"==msg) break;
+
+		sample.id(id++);
+		sample.message(msg);
+		dw.write(sample);
+		std::cout<<"DW:"<<sample<<" done"<<std::endl;
+	}
 
 	std::cout<<"pub end"<<std::endl;
 	return 0;
