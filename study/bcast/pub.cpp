5de8e0d4daa708c91a00e0ee9a044e166e203929
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-14 11:53:22 +0900

    add code which detects signal SIGINT(Ctrl-c) and exit program

--- study/bcast/pub.cpp
+++ study/bcast/pub.cpp
@@ -2,6 +2,12 @@
 #include <gen/bcast_DCPS.hpp>
 #include <util.h>
 
+void exitHandler(int s)
+{
+	std::cout<<"Signal:"<<s<<std::endl;
+	exit(1);
+}
+
 int main(int argc,char* argv[])
 {
 	if(argc<2) {
@@ -19,6 +25,12 @@ int main(int argc,char* argv[])
 	bcast::Data sample;
 	for(unsigned long i=0;i<payloadSize;i++) sample.payload().push_back(0x0);
 	std::cout<<"sample payload size="<<sample.payload().size()<<std::endl;
+
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = exitHandler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT,&sigIntHandler,NULL);
 
 	unsigned long long id=0;
 	std::string msg;
